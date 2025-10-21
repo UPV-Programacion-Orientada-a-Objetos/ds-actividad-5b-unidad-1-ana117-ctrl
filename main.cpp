95f319f7996017e038832ed58f8e2e157f3557a1
ana117-ctrl <2430118@upv.edu.mx>
2025-10-20 18:58:06 -0600

    Estructura base del proyecto

--- main.cpp
+++ main.cpp
@@ -1,5 +1,31 @@
 #include <iostream>
+#include <iomanip>
 
+using namespace std;
+
+//clase base abstracta 
+template <typename T>
+class MatrizBase {
+protected:
+    int _filas;
+    int _columnas;
+
+public:
+    MatrizBase(int filas, int columnas) : _filas(filas), _columnas(columnas) {}
+    virtual ~MatrizBase() {}
+    
+    virtual void cargarValores() = 0;
+    virtual MatrizBase<T>* sumar(const MatrizBase<T>& otra) const = 0;
+    virtual void imprimir() const = 0;
+    
+    int getFilas() const { return _filas; }
+    int getColumnas() const { return _columnas; }
+};
+
+//Main
 int main() {
+    cout << "--- Sistema Genérico de Álgebra Lineal ---" << endl;
+    cout << ">> Demostración de Genericidad (Tipo FLOAT) <<"<< endl;
+    
     return 0;
 }
